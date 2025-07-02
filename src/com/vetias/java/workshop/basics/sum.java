26f93771b395b63f446f329b98933e63fd7d363b
Abarnahashini <abarnas24cse@vetias.ac.in>
2025-07-02 15:35:04 +0530

    total marks program

--- src/com/vetias/java/workshop/basics/sum.java
+++ src/com/vetias/java/workshop/basics/sum.java
@@ -1,7 +1,7 @@
 package com.vetias.java.workshop.basics;
 public class sum {
     public static void main(String[] args){
-        int[] marks = {90, 80, 70, 60, 50};
+        int[] marks = {98, 80, 70, 60, 50};
         int sums=0;
     for (int mark : marks) {
         sums+=mark;
