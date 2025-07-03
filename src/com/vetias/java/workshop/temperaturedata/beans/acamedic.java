9e83cacfd4cdd856128f34680a1f548edd15fda8
Abarnahashini <abarnas24cse@vetias.ac.in>
2025-07-03 15:59:07 +0530

    Temperature data

---
+++ src/com/vetias/java/workshop/temperaturedata/beans/acamedic.java
@@ -0,0 +1,10 @@
+package src.com.java.workshop.temperaturedata.beans;
+
+public class acamedic extends Building {
+    private boolean islibrary;
+    private boolean isauditoriumavailable;
+    
+}
+
+
+
