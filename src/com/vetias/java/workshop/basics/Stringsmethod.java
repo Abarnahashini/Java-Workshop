cfaf0d089b03b12e7d06e92de24ded4d9a89d470
Abarnahashini <abarnas24cse@vetias.ac.in>
2025-07-07 10:24:33 +0530

    basic program in string concept

---
+++ src/com/vetias/java/workshop/basics/Stringsmethod.java
@@ -0,0 +1,14 @@
+package com.vetias.java.workshop.basics;
+
+public class Stringsmethod {
+    public static  void main(String[]args){
+        String firstname ="Abarna";
+        String lastname ="hashini";
+        String Name= firstname.concat(lastname);
+        System.out.println(Name);
+         String finalname=Name.toUpperCase();
+        System.out.println(finalname);
+    
+    }
+    
+}
