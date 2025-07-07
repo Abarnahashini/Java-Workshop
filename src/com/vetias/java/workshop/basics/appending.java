94aa551a869b8c326dfc9f3890e0d3330883eba6
Abarnahashini <abarnas24cse@vetias.ac.in>
2025-07-07 14:25:21 +0530

    appending the string

---
+++ src/com/vetias/java/workshop/basics/appending.java
@@ -0,0 +1,17 @@
+package com.vetias.java.workshop.basics;
+
+public class appending {
+    public static void main (String[] args){
+        StringBuilder sentance=new StringBuilder();
+        sentance.append("java ");
+         sentance.append("is ");
+          sentance.append("the ");
+           sentance.append("most ");
+            sentance.append("used ");
+             sentance.append("language ");
+              sentance.append("for ");
+         sentance.append("enterprise ");
+          sentance.append("Application ");
+          System.out.println(sentance.toString());
+    }
+}
