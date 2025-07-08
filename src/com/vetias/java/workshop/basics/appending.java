9091704b10d0259022403ab75b9431d7d49826a3
Abarnahashini <abarnas24cse@vetias.ac.in>
2025-07-08 10:42:44 +0530

    appending programs

--- src/com/vetias/java/workshop/basics/appending.java
+++ src/com/vetias/java/workshop/basics/appending.java
@@ -4,14 +4,14 @@ public class appending {
     public static void main (String[] args){
         StringBuilder sentance=new StringBuilder();
         sentance.append("java ");
-         sentance.append("is ");
-          sentance.append("the ");
-           sentance.append("most ");
-            sentance.append("used ");
-             sentance.append("language ");
-              sentance.append("for ");
-         sentance.append("enterprise ");
-          sentance.append("Application ");
-          System.out.println(sentance.toString());
+        sentance.append("is ");
+        sentance.append("the ");
+        sentance.append("most ");
+        sentance.append("used ");
+        sentance.append("language ");
+        sentance.append("for ");
+        sentance.append("enterprise ");
+        sentance.append("Application ");
+        System.out.println(sentance.toString());
     }
 }
