b9112fbeb38f3af2519978ed3c261cb911140f8c
Abarnahashini <abarnas24cse@vetias.ac.in>
2025-07-08 10:51:21 +0530

    Resgistration timeleft programs

--- src/com/vetias/java/workshop/basics/RegistrationTimeLeft.java
+++ src/com/vetias/java/workshop/basics/RegistrationTimeLeft.java
@@ -4,11 +4,11 @@ import java.time.LocalTime;
 public class RegistrationTimeLeft {
     public static void main(String[] args) {
         LocalTime currenttime=LocalTime.now();
-        LocalTime registrationendtime=LocalTime.of(6, 00, 00);
+        LocalTime registrationendtime=LocalTime.of(18, 59, 59);
         int hoursleft=registrationendtime.getHour()-currenttime.getHour();
         int minleft=registrationendtime.getMinute()-currenttime.getMinute();
         int secondleft=registrationendtime.getSecond()-currenttime.getSecond();
-        System.out.println("you have "+hoursleft+"hours "+minleft+"minutes "+secondleft+"seconds lef");
+        System.out.println("you have "+hoursleft+"hours "+minleft+"minutes "+secondleft+"seconds left");
 
     }}
     
