741427554ad97274d36c80775c5af58c70afd330
Abarnahashini <abarnas24cse@vetias.ac.in>
2025-07-09 10:37:58 +0530

    reading a file

---
+++ src/com/vetias/java/workshop/standardinput/CharacterBasedReaderExample.java
@@ -0,0 +1,25 @@
+package com.vetias.java.workshop.standardinput;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class CharacterBasedReaderExample {
+    public static void main(String[] args) {
+        try (BufferedReader reader= new BufferedReader(new FileReader("README.md"))){
+            String line;
+            while((line=reader.readLine())!=null){
+                System.out.println(line);
+
+            }
+        }
+            catch(IOException e){
+                System.err.println("an eror occured while reading thefile"+e.getMessage());
+            } 
+
+        
+
+    }}
+    
+
